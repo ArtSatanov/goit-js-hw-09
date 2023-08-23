7c80d34e7a0b6200300f17ca29089acef7183cea
Artem Satanovskiy <artsatanov@gmail.com>
2023-08-23 23:52:13 +0300

    test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const refs  = {
+   start: document.querySelector('button[data-start]'),
+   stop: document.querySelector('button[data-stop]'),
+}
+
+console.log(refs.start, refs.stop);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
 
