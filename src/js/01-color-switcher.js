e96816ac974de8c45a941f5f42a431166743b827
ArtSatanov <artsatanov@gmail.com>
2023-08-24 01:19:20 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,24 @@
 const refs  = {
    start: document.querySelector('button[data-start]'),
-   stop: document.querySelector('button[data-stop]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
 }
 
-console.log(refs.start, refs.stop);
+console.log(refs.body);
 
+let timerId = null; 
+
+refs.start.addEventListener('click', () => {
+  timerId = setInterval(() => {refs.body.style.backgroundColor = getRandomHexColor()}, 1000);
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+});
+
+refs.stop.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
