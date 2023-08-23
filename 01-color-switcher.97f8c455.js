c13b78ce66b16738d8d761b31e09aa51ca177bb3
ArtSatanov <105995279+ArtSatanov@users.noreply.github.com>
2023-08-23 20:52:37 +0000

    Deploying to gh-pages from @ ArtSatanov/goit-js-hw-09@7c80d34e7a0b6200300f17ca29089acef7183cea 🚀

---
+++ 01-color-switcher.97f8c455.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};console.log(t.start,t.stop);
+//# sourceMappingURL=01-color-switcher.97f8c455.js.map
---
+++ 01-color-switcher.a5f71b93.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};console.log(t.start,t.stop)}();
+//# sourceMappingURL=01-color-switcher.a5f71b93.js.map
