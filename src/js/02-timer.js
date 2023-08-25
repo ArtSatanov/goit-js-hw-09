ab6e0aa08413436d223e4c13ff67b4cc63d58151
Artem Satanovskiy <artsatanov@gmail.com>
2023-08-25 14:55:32 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -20,7 +20,6 @@ const options = {
   onClose(selectedDates) {
     console.log(selectedDates[0]);
     
-
     if (selectedDates[0] < options.defaultDate) {
       Notiflix.Notify.failure("Please choose a date in the future");
       refs.start.disabled = true;
@@ -48,9 +47,6 @@ const options = {
   }, 1000);
       });
     };
-
-    
-
   },
 };
 
@@ -80,10 +76,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-
-// addLeadingZero(value) > padStart()
