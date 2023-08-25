b59d78310bc06c9ccc302ca9e70aeec3e90138b7
Artem Satanovskiy <artsatanov@gmail.com>
2023-08-25 16:19:02 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  firstDelay: document.querySelector('input[name="delay"]'),
+  stepDelay: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  submit: document.querySelector('button[type="submit"]'),
+};
+
+refs.submit.addEventListener('click' , onSubmit())
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    
   } else {
     // Reject
   }
+};
+
+
+function onSubmit(firstDelay, stepDelay, amount) {
+  const promArray = [];
+  for (let i = 1; i <= amount, i += 1) {
+    
+  //  promArray.push(createPromise(i,))
+  }
+  
 }
+
